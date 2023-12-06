ad8c4d957c9a9d4486c2ad1a07f4ddd852f50401
The Bible-smith Project <boanuge@users.noreply.github.com>
2023-12-06 19:06:55 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ if __name__ == "__main__":
     # Setup DBQA
     start = timeit.default_timer()
     dbqa = setup_dbqa()
-    response = dbqa({'query': 'How much is the minimum guarantee payable by adidas?'})
+    #response = dbqa({'query': 'How much is the minimum guarantee payable by adidas?'})
+    #response = dbqa({'query': '예수님이 누구신가요?'}) # 한국어 인식은 잘 안되는듯함
+    response = dbqa({'query': 'Who is Jesus?'})
     end = timeit.default_timer()
 
     print(f'\nAnswer: {response["result"]}')
@@ -19,8 +21,8 @@ if __name__ == "__main__":
     for i, doc in enumerate(source_docs):
         print(f'\nSource Document {i+1}\n')
         print(f'Source Text: {doc.page_content}')
-        print(f'Document Name: {doc.metadata["source"]}')
-        print(f'Page Number: {doc.metadata["page"]}\n')
+        print(f'Document Name: {doc.metadata["source"]}\n')
+        #print(f'Page Number: {doc.metadata["page"]}\n')
         print('='* 60)
 
     print(f"Time to retrieve response: {end - start}")
